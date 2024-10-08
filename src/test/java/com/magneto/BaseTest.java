e57b9936be673f6e739e7ffc0174f9f0d7a6e494
deloire <lazzero@MacBook-Air.local>
2024-10-08 18:45:28 +0300

    Configurate BaseTest class

---
+++ src/test/java/com/magneto/BaseTest.java
@@ -0,0 +1,28 @@
+package com.magneto;
+
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+abstract public class BaseTest {
+
+
+    public void setUp() {
+        Configuration.browser = "chrome";
+        Configuration.browserSize = "1280x720";
+        Configuration.headless = false;
+        Configuration.holdBrowserOpen = true;
+    }
+
+    @BeforeEach
+    public void init(){
+        setUp();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        Selenide.closeWebDriver();
+    }
+}
